eb3f15dbe1394bdad6d8a44e570cfbf3570e0d49
Akshay Chormule <akshaychormule61@gmail.com>
2022-02-19 17:33:14 +0530

    Even Odd Program

---
+++ HomeWork1-3Proj/src/mypackage/EvenOdd.java
@@ -0,0 +1,16 @@
+package mypackage;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 32;
+		if(a%2 ==0) {
+			System.out.println("Number is even");
+		}else {
+			System.out.println("Number is odd");
+		}
+
+	}
+
+}
